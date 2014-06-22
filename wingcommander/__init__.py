d8e91c814f1ea0d4a31e42a9049b0dfec6ae5fd1
Jeff <jostendorf@gmail.com>
2014-06-22 10:59:45 -0500

    Added `tablize` "function"
    Using a class instead to attach associated predefined values to the instance.

--- wingcommander/__init__.py
+++ wingcommander/__init__.py
@@ -1,5 +1,5 @@
 from commander import WingCommander
-from util import run
+from util import run, watch, tablize
 
 __version__ = "0.1"
-__all__ = ["WingCommander", "run"]
+__all__ = ["WingCommander", "run", "watch", "tablize"]
--- wingcommander/util.py
+++ wingcommander/util.py
@@ -1,3 +1,6 @@
+# -*- coding: utf8 -*-
+
+
 def gen_prompt(cmd_):
     tree = [cmd_.name]
     cmd = cmd_.parent
@@ -67,5 +70,66 @@ def watch(func, wait=1):
         return
 
 
-def tablize(data):
-    return ""
+class Tablize(object):
+    DIVIDERS = ["|", "", ""]
+    BOX_DIVIDERS = ["│", "─", "┼"]
+    CENTER = 'c'
+    LEFT = 'l'
+    RIGHT = 'r'
+    COLUMN_TEMPLATE = " {{:{0}{1}.{1}}} "
+
+    @classmethod
+    def __call__(cls, data, max_length=-1, keys=None,
+                 dividers=DIVIDERS, labels=None, alignment=None):
+        if isinstance(data[0], dict):
+            data = cls.extract_dicts(data, keys)
+
+        sizes = cls.calc_columns(data, max_length)
+
+        frmt = []
+        row_divider = ""
+        for i in range(len(sizes)):
+            frmt.append(cls.COLUMN_TEMPLATE.format(
+                cls.translate_alignment(alignment[i] if alignment else None),
+                str(sizes[i])))
+        frmt = (dividers[0] if dividers[0] else ' ').join(frmt)
+
+        if dividers[1]:
+            row_divider = (dividers[2] if dividers[2] else dividers[1]).join(
+                [(s+2) * dividers[1] for s in sizes])
+
+        output = [frmt.format(*labels)] if labels else []
+
+        for d in data:
+            if output and row_divider:
+                output.append(row_divider)
+            output.append(frmt.format(*d))
+
+        return output
+
+    @classmethod
+    def extract_dicts(cls, data, keys):
+        if keys is None:
+            keys = data[0].keys()
+
+        return [[d.get(k, '') for k in keys] for d in data]
+
+    @classmethod
+    def calc_columns(cls, data, max_length):
+        sizes = map(
+            lambda x: max(*map(len, [d[x] for d in data])),
+            range(len(data[0])))
+
+        if isinstance(max_length, list):
+            return [min(max_length[i], sizes[i]) for i in range(len(sizes))]
+        elif max_length > 0:
+            return map(lambda x: min(x, max_length), sizes)
+
+        return sizes
+
+    @classmethod
+    def translate_alignment(cls, alignment="l"):
+        return "<" if alignment == "l" else \
+            "^" if alignment == "c" else ">"
+
+tablize = Tablize()
